472b9ed39eeb8102f94ac5b9a62cef1c973cbd97
Paul Baecher <pbaecher@gmail.com>
2016-03-27 23:35:10 +0200

    refactor state mgmt

---
+++ app/src/main/java/pb/gtd/State.java
@@ -0,0 +1,8 @@
+package pb.gtd;
+
+import java.util.ArrayList;
+
+public class State {
+    private ArrayList<Item> items = new ArrayList<Item>();
+    private ArrayList<String> tagOrder = new ArrayList<String>();
+}
